c2c98549c048095cefdbf7317a8c0104adb32ced
Lucas Souza <lucassouzzamarques@gmail.com>
2023-11-26 02:17:54 -0300

    shipment collection and delivery

---
+++ Rush 'n Deliver/Assets/Scripts/Shipment.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shipment : MonoBehaviour
+{
+    bool hasPackage = false;
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if((other.gameObject.tag == "Shipment") && (!hasPackage))
+        {
+            Debug.Log("Shipment picked up!");
+            hasPackage = true;
+            Destroy(other.gameObject, 0.8f);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        if((other.gameObject.tag == "Customer") && (hasPackage))
+        {
+            Debug.Log("Shipment delivered!");
+            hasPackage = false;
+        }      
+    }
+}
