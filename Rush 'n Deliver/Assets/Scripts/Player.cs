44c6b6257a677fcfbff9a19dead8198e05d59f3e
Lucas Souza <lucassouzzamarques@gmail.com>
2023-11-28 02:14:37 -0300

    player speed increase

--- Rush 'n Deliver/Assets/Scripts/Player.cs
+++ Rush 'n Deliver/Assets/Scripts/Player.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private float originalSpeed;
+    private float boostSpeed = 3.5f;
+    private float timeBoost = 0f;
+    private float boostDuration = 5f;
+    private bool isBoosted = false;
+
     [SerializeField] float moveSpeed = 1.5f;
     [SerializeField] float rotationSpeed = 300f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        originalSpeed = moveSpeed;
     }
 
     // Update is called once per frame
@@ -21,5 +27,26 @@ public class Player : MonoBehaviour
 
         transform.Rotate(0, 0, -rotationAmount);
         transform.Translate(0, moveAmount, 0);
+
+        if(isBoosted)
+        {
+            timeBoost += Time.deltaTime;
+
+            if(timeBoost >= boostDuration)
+            {
+                isBoosted = false;
+                moveSpeed = originalSpeed;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.gameObject.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+            isBoosted = true;
+            timeBoost = 0f;
+        }    
     }
 }
